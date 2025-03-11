b90d45f90991054effdfbc580c0b8403a50646f4
msharifabdalla2 <msharifabdalla1@yahoo.com>
2025-03-11 10:50:50 +0000

    Implement GET /api/topics endpoint

---
+++ controllers/topics.controller.js
@@ -0,0 +1,13 @@
+const {
+    fetchAllTopics
+} = require("../models/topics.model");
+
+exports.getTopics = (req, res, next) => {
+    fetchAllTopics()
+        .then((topics) => {
+            res.status(200).send({ topics });
+        })
+        .catch((err) => {
+            next(err);
+        });
+};

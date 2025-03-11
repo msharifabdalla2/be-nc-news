a5dd519c6b79f4e43fd87db82102ed6812e4592b
msharifabdalla2 <msharifabdalla1@yahoo.com>
2025-03-11 16:31:45 +0000

    Implement GET /api/articles/:article_id/comments

--- errors.controller/errors.controller.js
+++ errors.controller/errors.controller.js
@@ -8,7 +8,6 @@ exports.handlePsqlErrors = (err, req, res, next) => {
 };
 
 exports.handleCustomError = (err, req, res, next) => {
-    // console.log(err);
     if (err.status && err.msg) {
         res.status(err.status).send({ msg: err.msg })
     }

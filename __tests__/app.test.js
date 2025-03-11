b8d41dd922d7f9b93fa6a4dc1ef5922eb48c7473
msharifabdalla2 <msharifabdalla1@yahoo.com>
2025-03-11 09:47:08 +0000

    Implemented GET /api endpoint with test coverage

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,26 @@
 const endpointsJson = require("../endpoints.json");
-/* Set up your test imports here */
+const db = require("../db/connection");
+const data = require("../db/data/test-data");
+const request = require("supertest");
+const seed = require("../db/seeds/seed");
+const app = require("../app");
+require('jest-sorted');
 
-/* Set up your beforeEach & afterAll functions here */
+beforeEach(() => {
+  return seed(data);
+});
 
-describe.skip("GET /api", () => {
+afterAll(() => {
+  return db.end();
+});
+
+describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
     return request(app)
       .get("/api")
       .expect(200)
-      .then(({ body: { endpoints } }) => {
-        expect(endpoints).toEqual(endpointsJson);
+      .then(({ body }) => {
+        expect(body).toEqual(endpointsJson);
       });
   });
 });

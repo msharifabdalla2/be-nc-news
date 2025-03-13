b8771e8e04eacf32f25c36ba90c8312684d106c3
msharifabdalla2 <msharifabdalla1@yahoo.com>
2025-03-13 15:44:38 +0000

    Set up production environment and update package.json

--- db/connection.js
+++ db/connection.js
@@ -2,15 +2,25 @@ const { Pool } = require("pg");
 
 const ENV = process.env.NODE_ENV || 'development'
 
-require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
+require('dotenv').config({ path: `${__dirname}/../.env.${ENV}` })
 
-const db = new Pool();
+const config = {};
 
-if (!process.env.PGDATABASE) {
-    throw new Error("No PGDATABASE configured")
-} else { 
-    console.log(`Connected to ${process.env.PGDATABASE}`)
+// Check if the environment is production and set up the database configuration
+if (ENV === "production") {
+    if (!process.env.DATABASE_URL) {
+        throw new Error("DATABASE_URL not set for production");
+    }
+    config.connectionString = process.env.DATABASE_URL;
+    config.max = 2; // Limit the number of database connections
+} else {
+    if (!process.env.PGDATABASE) {
+        throw new Error("No PGDATABASE configured");
+    }
+    console.log(`Connected to ${process.env.PGDATABASE}`);
 }
 
+// Create a new Pool with the config (either production or development/test)
+const db = new Pool(config);
 
 module.exports = db;

046bcac5e25d5f20d1cbe7d1b83715f6892318fb
msharifabdalla2 <msharifabdalla1@yahoo.com>
2025-03-14 15:15:16 +0000

    Add GET /api/users/:username endpoint

--- models/users.model.js
+++ models/users.model.js
@@ -7,3 +7,28 @@ exports.fetchAllUsers = () => {
             return rows;
         });
 };
+
+exports.fetchUserByUsername = (username) => {
+    if (!isNaN(username)) {
+        return Promise.reject(({
+            status: 400,
+            msg: "Invalid username format"
+        }));
+    };
+
+    return db
+        .query(
+            `SELECT * FROM users WHERE username = $1`,
+            [username]
+        )
+        .then(({ rows }) => {
+            const user = rows[0]
+            if (!user) {
+                return Promise.reject({
+                    status: 404,
+                    msg: `No user found with username: ${username}`
+                });
+            };
+            return user;
+        });
+};
